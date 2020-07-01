6016e9b59f253a6d32b596d8460b992a840d54d1
DwieGodziny <inalosenspace2683@gmail.com>
2020-07-01 21:13:38 +0200

    Core layer

---
+++ Server/Core/Domain/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace Core.Domain
+{
+    /// <summary>
+    ///     Represents the user entity.
+    /// </summary>
+    public class User
+    {
+        #region Properties
+
+        /// <summary>
+        ///     The user ID.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        ///     The username.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        ///     The user password
+        /// </summary>
+        public SecureString Password { get; set; }
+
+        #endregion
+    }
+}
